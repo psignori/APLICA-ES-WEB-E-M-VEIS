4d3744f1c0751c6f755e4f07b29636e26c6cc513
psignori <64922204+psignori@users.noreply.github.com>
2022-09-05 20:44:26 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+// função que é chamada para mostrar a div na tela
+function openWindow () {
+    document.getElementById("janela").style.display = 'block'; 
+}
+
+// função que é chamada para ocultar a div a dar e recarregar a tela após isso
+function closeWindow () {
+    document.getElementById("janela").style.display = 'none';
+    window.location.reload(); 
+}
